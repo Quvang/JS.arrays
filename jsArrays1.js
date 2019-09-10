feb3a624d6232c3db316b8b0c02d5286b39a2b44
Quvang <suneq18@gmail.com>
2019-09-10 11:24:47 +0200

    Tilføjet to filer til aflevering

---
+++ jsArrays1.js
@@ -0,0 +1,170 @@
+'use strict'
+
+let arrRoll = [0, 0, 0, 0, 0, 0, 0];
+let equal;
+
+function roll(n) {
+  return Math.floor(Math.random() * n + 1);
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+equal = roll(6);
+
+if (equal === 1) {
+  arrRoll[1]++
+} else if (equal === 2) {
+  arrRoll[2]++
+} else if (equal === 3) {
+  arrRoll[3]++
+} else if (equal === 4) {
+  arrRoll[4]++
+} else if (equal === 5) {
+  arrRoll[5]++
+} else if (equal === 6) {
+  arrRoll[6]++
+}
+
+console.log(arrRoll);
---
+++ myFuncLib.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const roll = function(n) {
+  return Math.floor(Math.random() * n + 1);
+}
